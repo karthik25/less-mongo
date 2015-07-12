b48ac02e95e8291f2bb1ae7c15ca4cf96af8fedc
Karthik Ananthapadmanaban <karthika25@gmail.com>
2015-07-12 17:22:42 -0400

    grunt for concatenating files

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+// Gruntfile.js
+
+// our wrapper function (required by grunt and its plugins)
+// all configuration goes inside this function
+module.exports = function(grunt) {
+	// Project configuration.
+	grunt.initConfig({
+		// Metadata.
+		pkg: grunt.file.readJSON('package.json'),
+		// Task configuration.
+		concat: {
+			options: {
+				stripBanners: true
+			},
+			dist: {
+				src: [
+					'src/less.mongo.js',
+					'src/less.iterables.js'
+				],
+				dest: 'less.mongo.js'
+			}
+		},
+		jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				unused: true,
+				boss: true,
+				eqnull: true,
+				globals: {
+					jQuery: true
+				},
+				node: true
+			},
+			gruntfile: {
+				src: 'Gruntfile.js'
+			}
+		}
+	});
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-text-replace');
+
+	// Default task.
+	grunt.registerTask('default', ['jshint', 'concat']);
+};
